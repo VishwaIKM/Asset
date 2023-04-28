b224f79725b5c534ff4fcd1c4783144862c7c8e6
Vishwa <vishwa@gmail.com>
2023-04-28 14:33:24 +0530

    new entry and update entry design added

--- AssetManagementVishwa/ViewModel/Dashboard_vm.cs
+++ AssetManagementVishwa/ViewModel/Dashboard_vm.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Architecturalpattern_mvvm;
+using AssetManagementVishwa.View.Pages;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AssetManagementVishwa.ViewModel
 {
@@ -14,6 +18,26 @@ namespace AssetManagementVishwa.ViewModel
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private RelayCommand edit_clickCommand;
+
+        public ICommand Edit_clickCommand
+        {
+            get
+            {
+                if (edit_clickCommand == null)
+                {
+                    edit_clickCommand = new RelayCommand(Edit_click);
+                }
+
+                return edit_clickCommand;
+            }
+        }
+
+        private void Edit_click()
+        {
+            UpdateEntry_p _edit_page = new UpdateEntry_p();
+            
+        }
         #endregion
     }
 }
--- AssetManagementVishwa/ViewModel/NewEntry_vm.cs
+++ AssetManagementVishwa/ViewModel/NewEntry_vm.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AssetManagementVishwa.ViewModel
 {
-    internal class NewEntry_vm
+    internal class NewEntry_vm : INotifyPropertyChanged
     {
+
+        private string _selectedItem;
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        private ObservableCollection<string> _items = new ObservableCollection<string>(){
+            "V",
+            "Vi",
+            "Vis",
+            "Vish",
+            "Vishw",
+            "Vishwa",
+        };
+        public IEnumerable Items
+        {
+            get { return _items; }
+        }
+
+        public string SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged("SelectedItem");
+            }
+        }
+
+        public string NewItem
+        {
+            set
+            {
+                if (SelectedItem != null)
+                {
+                    return;
+                }
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _items.Add(value);
+                    SelectedItem = value;
+                }
+            }
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        private string sysUser;
+
+        public string SysUser { get => sysUser; set => SetProperty(ref sysUser, value); }
     }
 }
